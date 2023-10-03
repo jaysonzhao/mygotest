23bf420aeb61508dbb158e68e7ad763084a27796
Jaysonzhao <jaysonzhao@vip.qq.com>
2023-10-03 21:12:22 +0800

    first commit

---
+++ api/api.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"github.com/jaysonzhao/gotest/handlers"
+
+	"github.com/labstack/echo"
+)
+
+func MainGroup(e *echo.Echo) {
+	// Route / to handler function
+	e.GET("/health-check", handlers.HealthCheck)
+
+	e.GET("/cats/:data", handlers.GetCats)
+	e.POST("/cats", handlers.AddCat)
+
+}
+
+func AdminGroup(g *echo.Group) {
+	g.GET("/main", handlers.MainAdmin)
+}
---
+++ handlers/healthcheck.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/jaysonzhao/gotest/vo"
+
+	"github.com/labstack/echo"
+)
+
+// HealthCheck - Health Check Handler
+func HealthCheck(c echo.Context) error {
+	//requestID := uuid.NewV4()
+	//c.Logger().Infof("RequestID: %s", requestID)
+
+	resp := vo.HealthCheckResponse{
+		Message: "Everything is good!",
+	}
+	return c.JSON(http.StatusOK, resp)
+}
